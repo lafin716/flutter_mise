eea7960238c45a7fb709b6adc801c2856c0f3fc5
lafin716 <lafin716@gmail.com>
2022-08-29 23:06:40 +0900

    feat: 미세먼지 API 모델링

---
+++ lib/repository/stat_repository.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_mise/const/data.dart';
+import 'package:flutter_mise/model/stat_model.dart';
+
+class StatRepository {
+  static Future<List<StatModel>> fetchData() async {
+    final response = await Dio().get(
+      'http://apis.data.go.kr/B552584/ArpltnStatsSvc/getCtprvnMesureLIst',
+      queryParameters: {
+        'serviceKey': serviceKey,
+        'returnType': 'json',
+        'pageNo': 1,
+        'numOfRows': 30,
+        'itemCode': 'PM10',
+        'dataGubun': 'HOUR',
+        'searchCondition': 'WEEK',
+      },
+    );
+
+    return response.data['response']['body']['items']
+        .map<StatModel>(
+          (response) => StatModel.fromJson(
+            json: response,
+          ),
+        )
+        .toList();
+  }
+}
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,12 +1,33 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mise/component/category_card.dart';
 import 'package:flutter_mise/component/hourly_card.dart';
 import 'package:flutter_mise/component/main_app_bar.dart';
 import 'package:flutter_mise/component/main_drawer.dart';
 import 'package:flutter_mise/const/colors.dart';
+import 'package:flutter_mise/const/data.dart';
+import 'package:flutter_mise/model/stat_model.dart';
+import 'package:flutter_mise/repository/stat_repository.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  fetchData() async {
+    final statModels = await StatRepository.fetchData();
+
+    print(statModels);
+  }
 
   @override
   Widget build(BuildContext context) {
